a15cb6dc792cebbb1be313ab1011598c59037a97
AnanasPizzaMigliore <90773548+AnanasPizzaMigliore@users.noreply.github.com>
2025-10-03 22:26:28 +0200

    Use helper for RCNN training mode

--- python/jdet/models/networks/rcnn.py
+++ python/jdet/models/networks/rcnn.py
@@ -1,8 +1,11 @@
 from math import e
+
 import torch
 from torch import nn
 
 from jdet.utils.registry import MODELS,build_from_cfg,BACKBONES,HEADS,NECKS
+
+from ._training import set_module_training_mode
 
 
 @MODELS.register_module()
@@ -52,12 +55,17 @@ class RCNN(nn.Module):
         return self.forward(images, targets)
 
     def train(self, mode: bool = True):
-        super(RCNN, self).train(mode)
-        self.backbone.train(mode)
-        if self.neck:
-            self.neck.train(mode)
-        if self.rpn:
-            self.rpn.train(mode)
-        if self.bbox_head:
-            self.bbox_head.train(mode)
+        try:
+            super().train(mode)
+        except TypeError:
+            super().train()
+
+        set_module_training_mode(self.backbone, mode)
+        if getattr(self, "neck", None):
+            set_module_training_mode(self.neck, mode)
+        if getattr(self, "rpn", None):
+            set_module_training_mode(self.rpn, mode)
+        if getattr(self, "bbox_head", None):
+            set_module_training_mode(self.bbox_head, mode)
+
         return self
